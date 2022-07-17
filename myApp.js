3d5ed40f8a2b6466755b1742086ee331278fdaf8
Michelle A <michelleameral@gmail.com>
2022-07-16 20:01:54 -0400

    Task 7 - Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -10,6 +10,11 @@ app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(function(req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
